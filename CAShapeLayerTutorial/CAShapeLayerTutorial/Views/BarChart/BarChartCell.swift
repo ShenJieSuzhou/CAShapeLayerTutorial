7ec781e0d36d72dffd02bd37e10c7e732a35da2e
ShenJieSuzhou <745689122@qq.com>
2021-11-17 18:02:44 +0800

    Add print function to print position.

--- CAShapeLayerTutorial/CAShapeLayerTutorial/Views/BarChart/BarChartCell.swift
+++ CAShapeLayerTutorial/CAShapeLayerTutorial/Views/BarChart/BarChartCell.swift
@@ -24,14 +24,19 @@ struct BarChartCell: View {
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 4)
-                .fill(LinearGradient(gradient: gradient?.getGradient() ?? GradientColor(start: accentColor, end: accentColor).getGradient(), startPoint: .bottom, endPoint: .top))
-                .frame(width: CGFloat(self.cellWidth))
-                .scaleEffect(CGSize(width: 1, height: self.scaleValue), anchor: .bottom)
-                .onAppear {
-                    self.scaleValue = self.value
-                }
-                .animation(.spring().delay(self.touchLocation < 0 ? Double(self.index) * 0.04 : 0))
+            GeometryReader { gr in
+                RoundedRectangle(cornerRadius: 4)
+                    .fill(LinearGradient(gradient: gradient?.getGradient() ?? GradientColor(start: accentColor, end: accentColor).getGradient(), startPoint: .bottom, endPoint: .top))
+                    .frame(width: CGFloat(self.cellWidth))
+                    .scaleEffect(CGSize(width: 1, height: self.scaleValue), anchor: .bottom)
+                    .onAppear {
+                        self.scaleValue = self.value
+                        
+                        print("local: \(gr.frame(in: .local))")
+                    }
+                    .animation(.spring().delay(self.touchLocation < 0 ? Double(self.index) * 0.04 : 0))
+                
+            }
         }
     }
 }
